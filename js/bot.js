4f6314314389cf74a58690899071319faeec3f0b
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-16 16:51:39 -0300

    setInterval and stop script

--- js/bot.js
+++ js/bot.js
@@ -5,13 +5,13 @@ if(location.href === "https://www.nike.com.br/Carrinho"){
 if(location.href === "https://www.nike.com.br/Checkout"){
     document.getElementById("seguir-pagamento")?.addEventListener("click", event => event.preventDefault())
     document.getElementById("seguir-pagamento")?.click();
-    const code = document.getElementsByClassName("ModalCorpoCentralizado ")[0]?.id.split("_")[1];
-    document.getElementsByName("btn0_modalNotice_"+code)[0]?.click();
-    document.getElementsByClassName("close")[1]?.click();
+    document.querySelector(".button.undefined").click();
+    // document.getElementsByClassName("close")[1]?.click();
     const [arrow] = document.getElementsByClassName("select-cta-arrow")
     arrow?.click()
     document.getElementsByName("ccidradio")[1]?.click()
     document.getElementById("politica-trocas")?.click()
+    // document.getElementById("confirmar-pagamento").click()
 }
 
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 function inject(){
-    chrome.tabs.executeScript({
-        file: './js/bot.js'
-    });
+    let i = 0;
+    const loop = setInterval(()=>{
+        if(i<=4){
+            chrome.tabs.executeScript({
+                file: './js/bot.js'
+            });
+        }
+        else clearInterval(loop)
+        i++
+    }, 1000)
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
