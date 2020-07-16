638fb0afe61cd5c5d6afa3150c243c71102dfa4b
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-16 17:46:14 -0300

    add new features

--- js/bot.js
+++ js/bot.js
@@ -5,13 +5,18 @@ if(location.href === "https://www.nike.com.br/Carrinho"){
 if(location.href === "https://www.nike.com.br/Checkout"){
     document.getElementById("seguir-pagamento")?.addEventListener("click", event => event.preventDefault())
     document.getElementById("seguir-pagamento")?.click();
-    document.querySelector(".button.undefined").click();
+    document.querySelector(".button.undefined")?.click();
     // document.getElementsByClassName("close")[1]?.click();
     const [arrow] = document.getElementsByClassName("select-cta-arrow")
     arrow?.click()
     document.getElementsByName("ccidradio")[1]?.click()
     document.getElementById("politica-trocas")?.click()
-    // document.getElementById("confirmar-pagamento").click()
+    
+    // while(location.href === "https://www.nike.com.br/Checkout"){
+    //    setTimeout(()=>{
+    //      document.getElementById("confirmar-pagamento")?.click()
+    //    }, 1500)
+    //}
 }
 
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function inject(){
     let i = 0;
     const loop = setInterval(()=>{
@@ -9,8 +10,19 @@ function inject(){
         else clearInterval(loop)
         i++
     }, 1000)
+    let str =   "....................../´¯/)\n"+
+                "....................,/¯../\n"+
+                ".................../..../\n"+
+                "............./´¯/'...'/´¯¯`·¸\n"+
+                "........../'/.../..../......./¨¯\\\n"+
+                "........('(...´...´.... ¯~/'...')\n"+
+                ".........\\.................'...../\n"+
+                "..........''...\\.......... _.·´\n"+
+                "............\..............(\n"+ 
+                "..............\\.............\\...\n"+
+                "PAU NO CU DO CUCA "
+    console.log(str)
 }
-
 document.addEventListener("DOMContentLoaded",()=>{
     document.querySelector("#submitSnkrsBot")?.addEventListener("click", inject);
 })
